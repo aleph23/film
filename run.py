433f2fbb1ebcc84f9f23e43cc459a324eedc8c24
Sebastian <sebastian@betzin.com>
2023-05-02 14:18:29 +0200

    added run.py

---
+++ run.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import numpy as np
+import tempfile
+import tensorflow as tf
+import mediapy
+from PIL import Image
+from eval import interpolator, util
+
+_UINT8_MAX_F = float(np.iinfo(np.uint8).max)
+INPUT_EXT = ['.png', '.jpg', '.jpeg']
+
+def predict(self, frame1, frame2, times_to_interpolate):
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    interpolator = interpolator.Interpolator("/pretrained_models/film_net/Style/saved_model", None)
+
+    # Batched time.
+    batch_dt = np.full(shape=(1,), fill_value=0.5, dtype=np.float32)
+
+    assert os.path.splitext(str(frame1))[-1] in INPUT_EXT and os.path.splitext(str(frame2))[-1] in INPUT_EXT, "Please provide png, jpg or jpeg images."
+
+    # make sure 2 images are the same size
+    img1 = Image.open(str(frame1))
+    img2 = Image.open(str(frame2))
+
+    assert img1.size == img2.size, "Images must be same size"
+ 
+    input_frames = [str(frame1), str(frame2)]
+
+    frames = list(util.interpolate_recursively_from_files(input_frames, times_to_interpolate, interpolator))
+    print('Interpolated frames generated, saving now as output video.')
+
+    ffmpeg_path = util.get_ffmpeg_path()
+    mediapy.set_ffmpeg(ffmpeg_path)
+    out_path = Path(tempfile.mkdtemp()) / "out.mp4"
+    mediapy.write_video(str(out_path), frames, fps=30)
+
+    return out_path
+
